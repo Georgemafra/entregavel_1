4f792cc035eba62362a0e6ea42a8ea86e39e349c
George Mafra <georgemafrinha@gmail.com>
2023-12-04 12:27:14 -0300

    Backend JS da calculadora

--- src/script.js
+++ src/script.js
@@ -1,11 +1,47 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Calculadora(expressao) {
+  const expressaoLimpa = expressao.trim();
+  const operador = expressaoLimpa.match(/[\+\-\*\/\^e]/); // Adicionado 'e' para exponenciação
+  
+  if (!operador) {
+    console.log("Operador inválido");
+    return null;
+  }
 
-//ESCREVA SEU CÓDIGO AQUI!!
+  let [num1, num2] = expressaoLimpa.split(operador[0]).map(parseFloat);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log("Operandos inválidos");
+    return null;
+  }
+
+  let resultado;
+
+  switch (operador[0]) {
+    case "+":
+      resultado = num1 + num2;
+      break;
+    case "-":
+      resultado = num1 - num2;
+      break;
+    case "*":
+      resultado = num1 * num2;
+      break;
+    case "/":
+      resultado = num1 / num2;
+      break;
+    case "^":
+    case "e": // Adicionado 'e' para exponenciação
+      resultado = Math.pow(num1, num2);
+      break;
+    default:
+      console.log("Operador inválido");
+      return null;
+  }
+
+  return resultado;
+}
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let visor = document.getElementById("visor");
 
@@ -18,13 +54,11 @@ function limpezatotal() {
 }
 
 function calcular() {
-  let numeros = visor.value.split(/[^0-9]/)
-  let operadores = visor.value.split(/[0-9]/)
-  let operador
-  operadores.forEach(x => {
-    if (x != "")
-      operador = x
-  });
+  const resultado = Calculadora(visor.value);
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  if (resultado !== null) {
+    visor.value = resultado;
+  } else {
+    visor.value = "Erro na expressão";
+  }
 }
